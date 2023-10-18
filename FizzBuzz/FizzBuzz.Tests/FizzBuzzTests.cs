fd4b8ba14fde4eab1cdde2ad4e33c3e644d5d055
Nazanin <n.abbasi.111@gmail.com>
2023-10-18 16:02:25 +0100

    Initial failing tests

---
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,57 @@
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        private readonly IFizzBuzzService _sut;
+
+        public FizzBuzzTests()
+        {
+            _sut = new FizzBuzzService();
+        }
+
+        [Fact]
+        public void ShowResult_ReturnsOneFizz()
+        {
+            //Arrange
+            var inputs = new int[] { 1, 2, 3 };
+
+            var expectedResult = new string[] { "1", "2", "Fizz" };
+
+            //Act
+            var result = _sut.ShowResult(inputs);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void ShowResult_ReturnsOneBuzz()
+        {
+            //Arrange
+            var inputs = new int[] { 5, 6, 7 };
+
+            var expectedResult = new string[] { "Buzz", "Fizz", "7" };
+
+            //Act
+            var result = _sut.ShowResult(inputs);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void ShowResult_ReturnsFizzBuzz()
+        {
+            //Arrange
+            var inputs = Enumerable.Range(1, 16);
+
+            var expectedResult = new string[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16" };
+
+            //Act
+            var result = _sut.ShowResult(inputs);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
