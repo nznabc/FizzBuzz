7be6dd791b37ee9c33dba5383390a5e957990211
Nazanin <n.abbasi.111@gmail.com>
2023-10-18 17:11:46 +0100

    Add Console App to print the FizzBuzz result for the range 1 to 100

---
+++ FizzBuzz/FizzBuzz.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using FizzBuzz;
+using FizzBuzz.Contracts;
+
+// Create an instance of a class that implements IFizBuzzService
+IFizzBuzzService service = new FizzBuzzService();
+
+// Create a list of strings to be processed
+List<int> inputs = Enumerable.Range(1, 100).ToList();
+
+// Call the ShowResult method and print the output array
+string[] output = service.ShowResult(inputs);
+
+Console.WriteLine(string.Join(", ", output));
